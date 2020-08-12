e35ac9a97e452b6e18b47b77561a6a611fc0357c
Craig Richards <craig@greywidget.co.uk>
2020-08-12 15:07:45 +0100

    day 5 code challenge data structures (forgot to commit yesterday)

---
+++ days/07-09-data-structures/code/cars.py
@@ -0,0 +1,46 @@
+cars = {
+    'Ford': ['Falcon', 'Focus', 'Festiva', 'Fairlane'],
+    'Holden': ['Commodore', 'Captiva', 'Barina', 'Trailblazer'],
+    'Nissan': ['Maxima', 'Pulsar', '350Z', 'Navara'],
+    'Honda': ['Civic', 'Accord', 'Odyssey', 'Jazz'],
+    'Jeep': ['Grand Cherokee', 'Cherokee', 'Trailhawk', 'Trackhawk']
+}
+
+
+def get_all_jeeps(cars=cars):
+    """return a comma  + space (', ') separated string of jeep models
+       (original order)"""
+    return ', '.join(cars.get('Jeep'))
+
+
+def get_first_model_each_manufacturer(cars=cars):
+    """return a list of matching models (original ordering)"""
+    return [value[0] for value in cars.values()]
+
+
+def get_all_matching_models(cars=cars, grep='trail'):
+    """return a list of all models containing the case insensitive
+       'grep' string which defaults to 'trail' for this exercise,
+       sort the resulting sequence alphabetically"""
+    selected = []
+    for models in cars.values():
+        for model in models:
+            if grep.lower() in model.lower():
+                selected.append(model)
+
+    return sorted(selected)
+
+
+def sort_car_models(cars=cars):
+    """return a copy of the cars dict with the car models (values)
+       sorted alphabetically"""
+    sorted_models = {}
+    for k, v in cars.items():
+        sorted_models[k] = sorted(v)
+    return sorted_models
+
+
+if __name__ == '__main__':
+    # print(get_all_jeeps())
+    # print(get_first_model_each_manufacturer())
+    print(sort_car_models())
